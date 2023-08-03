166e91c92c306ed946a454c97bdf7735c13b716e
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-03 15:50:23 +0700

    Pt_1_6: Completed task
    The program works correctly
    The string is split and then each word is capitalized

---
+++ Pt_1_6.py
@@ -0,0 +1,16 @@
+def capitalizefirstletter(string):
+    # Разбиваем строку на слова
+    words = string.split()
+    
+    # Применяем функцию str.capitalize к каждому слову
+    capitalizedwords = [word.capitalize() for word in words]
+    
+    # Соединяем слова обратно в строку
+    capitalizedstring = ' '.join(capitalizedwords)
+    
+    return capitalizedstring
+
+# Пример использования
+inputstring = input("Введите строку: ")
+result = capitalizefirstletter(inputstring)
+print(result)
