94fd641bf12a6883a53227f701af64fec1ed7b85
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-03 08:20:27 +0700

    Pt_1_2: Completed task
    User is asked to enter their name, then last name.
    After that strings get merged into one.
    And after that program outputs the full name and its length.

---
+++ Pt_1_2.py
@@ -0,0 +1,6 @@
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+
+full_name = last_name + " " + first_name
+print("Полное имя:", full_name)
+print("Длина строки:", len(full_name))
