b968df3f6ced37fdc29b5b54280547ad56dca00f
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-10 15:48:57 +0700

    Pt_2_4: Task Changed
    Pycodestyle change only

--- Pt_2_4.py
+++ Pt_2_4.py
@@ -3,17 +3,17 @@ b = float(input("Enter the coefficient b: "))
 c = float(input("Enter the coefficient c: "))
 
 # Calculate discriminant
-discriminant = b**2 - 4*a*c
+discriminant = b ** 2 - 4 * a * c
 
 # Calculate square root of discriminant
 if discriminant > 0:
-    root1 = (-b + discriminant**0.5) / (2*a)
-    root2 = (-b - discriminant**0.5) / (2*a)
+    root1 = (-b + discriminant ** 0.5) / (2 * a)
+    root2 = (-b - discriminant ** 0.5) / (2 * a)
     print("Two real and distinct roots:")
     print("Root 1:", root1)
     print("Root 2:", root2)
 elif discriminant == 0:
-    root = -b / (2*a)
+    root = -b / (2 * a)
     print("One real root:")
     print("Root:", root)
 else:
