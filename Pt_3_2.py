28565a96d07684c1c2e778df51584465a64a4b26
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-10 16:59:21 +0700

    Pt_3_2: Task Completed
    Forgot to use lambda expression. Had to make a new program

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,11 +1,3 @@
-def fibonacci(n):
-    fiblist = [0, 1]  # список, содержащий первые два числа Фибоначчи
-    while fiblist[-1] < n:
-        fiblist.append(fiblist[-1] + fiblist[-2])
-    return fiblist
+fibonacci = lambda n: [0, 1] + [fibonacci(i-2) + fibonacci(i-1) for i in range(2, n)]
 
-
-example = fibonacci(100)
-print(example)
-
-# Output: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+print(fibonacci(10))
