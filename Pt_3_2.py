3e40b4e44c655c9bccdaeffb89c0f60b0e90b7ee
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-10 16:42:52 +0700

    Pt_3_2: Task Completed
    The program uses lambda expression to print all fib. numbers

---
+++ Pt_3_2.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    fiblist = [0, 1]  # список, содержащий первые два числа Фибоначчи
+    while fiblist-1 < n:
+        fiblist.append(fiblist-1 + fiblist[-2])
+    return fiblist:-1
+
+example = fibonacci(100)
+print(example)
+
+# Output: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
