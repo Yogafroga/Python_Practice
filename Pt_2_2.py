464383556e6c89eeda043043978945185acfec68
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-10 07:52:09 +0700

    Pt_2_2: Task completed
    The program allows you to enter your own name of the new show
    This show is then displayed in the list of different shows

---
+++ Pt_2_2.py
@@ -0,0 +1,14 @@
+shows = ("Show 1", "Show 2", "Show 3", "Show 4")
+shows = list(shows)
+print("Current shows:")
+for show in shows:
+    print(show)
+
+newshow = input("Enter the name of a new show: ")
+position = int(input("Enter the position at which the show should be inserted (1 to 5): "))
+
+shows.insert(position - 1, newshow)
+shows = tuple(shows)
+print("Updated shows:")
+for show in shows:
+    print(show)
