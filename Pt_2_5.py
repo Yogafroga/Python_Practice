8126187879f1749206767e0fa7bad64a6c1be6db
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-10 15:50:05 +0700

    Pt_2_5: Task Changed
    Pycodestyle change only

--- Pt_2_5.py
+++ Pt_2_5.py
@@ -5,18 +5,19 @@ def startgame():
     wins = 0
     losses = 0
     while losses < 3 or wins < 3:
-        choice = input("Орёл или решка? (0 - орёл, 1 - решка, любое другое число - выход): ")
+        choice = input("Орёл или решка? (0 - орёл, "
+                       "1 - решка, любое другое число - выход): ")
         if choice == '0':
             userchoice = 'орёл'
         elif choice == '1':
             userchoice = 'решка'
         else:
             break
-        
+
         coin = random.choice(['орёл', 'решка'])
         print(f"Вы выбрали {userchoice}.")
         print(f"Монета выпала {coin}.")
-        
+
         if userchoice == coin:
             print("Вы выиграли!")
             wins += 1
