d596070449fc2d5369f04ffdc215bd9ddb1f07c8
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-10 07:23:20 +0700

    Pt_1_10: Task completed
    Pycodestyle changes
    The program itself wasn't modified

--- Pt_1_10.py
+++ Pt_1_10.py
@@ -1,4 +1,5 @@
 def format_number(num):
     return '{:,.2f}'.format(num)
-print(format_number(float(input('Enter a float number: '))))
 
+
+print(format_number(float(input('Enter a float number: '))))
