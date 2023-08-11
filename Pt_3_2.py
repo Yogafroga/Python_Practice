de9e91fe2ed17e53b319d89a3304aa673ded0e56
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-11 08:08:20 +0700

    Pt_3_2: Task Modified
    Pycodestyle UPD

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,5 +1,5 @@
 from functools import reduce
 
-fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])
+fib = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [0, 1])
 
 print(fib(10))
