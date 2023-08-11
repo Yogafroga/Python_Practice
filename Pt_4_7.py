f1922cf0be420b472d98cdabf7c14fcb08084bda
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-11 08:43:08 +0700

    Pt_4_7: Task Completed
    The program prints out a list of all possible permutations
    of the list of guven numbers

---
+++ Pt_4_7.py
@@ -0,0 +1,23 @@
+def permutations(lst):
+    # Base case: If the list is empty, return an empty list
+    if len(lst) == 0:
+        return []
+
+    # Base case: If the list contains only one element, return a list containing that element
+    if len(lst) == 1:
+        return [lst]
+
+    # Recursive case: Find permutations for all elements except the first one
+    all_permutations = []
+    for i in range(len(lst)):
+        current_element = lst[i]
+        remaining_elements = lst[:i] + lst[i+1:]
+        for p in permutations(remaining_elements):
+            all_permutations.append([current_element] + p)
+
+    return all_permutations
+
+
+# Test the function
+my_list = [1, 2, 3, 4, 5]
+print(permutations(my_list))
