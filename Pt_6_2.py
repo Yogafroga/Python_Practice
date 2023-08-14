785b25c22f00fefbeed751bee2d8ce2debadd202
Maksim Baryshev <maksim0baryshev@gmail.com>
2023-08-14 16:07:20 +0700

    Pt_6_2: Task Completed
    The program now writes only games related tags to a csv file.

--- Pt_6_2.py
+++ Pt_6_2.py
@@ -7,15 +7,21 @@ import csv
 class SteamSpider(CrawlSpider):
     name = "steam"
     allowed_domains = ['store.steampowered.com']
-    start_urls = ['https://store.steampowered.com/search/?category1=998&filter=globaltopsellers&ndl=1']
+    start_urls = ['https://store.steampowered.com/search/?ignore_preferences=1&untags=7948&category1=998&filter=globaltopsellers&ndl=1']
     custom_settings = {
-        'CLOSESPIDER_PAGECOUNT': 1500
+        'CLOSESPIDER_PAGECOUNT': 2700
     }
     rules = (
         Rule(LinkExtractor(allow=r'app/'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
+        if 'sub/' in response.url:
+            return
+        if 'dlc/' in response.url:
+            return
+        if 'ost/' in response.url:
+            return
         tags = response.xpath("//a[@class='app_tag']")
         tag_text = [tag.xpath("text()").get().strip() for tag in tags]
 
